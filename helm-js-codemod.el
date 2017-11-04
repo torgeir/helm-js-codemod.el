d4d8073b9d24f560cb9655a3dc2ed8ff6a990c1b
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2017-11-04 12:05:00 +0100

    Depend on helm-core rather than all of helm
    https://github.com/melpa/melpa/pull/5079#issuecomment-341864591

--- helm-js-codemod.el
+++ helm-js-codemod.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2017 @torgeir
 
 ;; Author: Torgeir Thoresen <@torgeir>
-;; Version: 1.0.0
+;; Version: 1.0.1
 ;; Keywords: helm js codemod region
 ;; Package-Requires: ((emacs "24.4") (helm "1.9.8") (js-codemod "1.0.0"))
 
@@ -28,7 +28,7 @@
 
 ;;; Code:
 
-(require 'helm)
+(require 'helm-core)
 (require 'js-codemod)
 
 (defvar helm-js-codemod-mod-dir nil "Folder to read mods from.")
